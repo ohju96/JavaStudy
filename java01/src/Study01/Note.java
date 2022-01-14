03b9fb4d76041c28c5794b5b06f6cf426b49370c
ohju96 <ohju96@naver.com>
2022-01-14 16:07:21 +0900

    접근 제한자와 생성자 공부

--- java01/src/Study01/Note.java
+++ java01/src/Study01/Note.java
@@ -3,9 +3,9 @@ package Study01;
 class NoteSetting {
 
     //노트 필드
-
     String color; //노트 색상
     int page; // 노트 장 수
+    private int index = 0;
 
     // 노트 장 수 증가
     void upPage(int value) {
@@ -16,6 +16,34 @@ class NoteSetting {
     void downPage(int value) {
         page -= value;
     }
+
+    //--------------------------------------------------------------
+
+    // 노트 인덱스를 올린다.
+    public void upIndex(int value) {
+        if (index + value > 200){
+            index = 200;
+        } else {
+            index += value;
+        }
+        System.out.println("노트의 인덱스 번호는 " + getIndex() + "입니다.");
+    }
+
+    public void downIndex(int value) {
+        if (index - value > 0){
+            index = 0;
+        } else {
+            index -= value;
+        }
+        System.out.println("노트의 인덱스 번호는 " + getIndex() + "입니다.");
+    }
+
+    public int getIndex(){
+        return index;
+    }
+
+    //--------------------------------------------------------------
+
 }
 
 public class Note{
@@ -36,5 +64,20 @@ public class Note{
         myNote2.downPage(5);
         System.out.println("2번 노트의 색상은 " + myNote2.color + "색입니다.");
         System.out.println("2번 노트의 페이지는" + myNote2.page + "페이지입니다.");
+
+        System.out.println("------------------------------------------------------");
+
+        //--------------------------------------------------------------
+
+        NoteSetting myNote3 = new NoteSetting();
+
+        myNote3.upIndex(100);
+        myNote3.upIndex(150);
+
+        myNote3.downIndex(50);
+        myNote3.downIndex(100);
+
+        //--------------------------------------------------------------
+
     }
 }
---
+++ java01/src/Study01/Pen.java
@@ -0,0 +1,35 @@
+package Study01;
+
+class PenSetting {
+    // 필드
+    private String color;
+    private int number;
+
+
+    PenSetting(String color, int number) {
+        this.color = color;
+        this.number = number;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    
+}
+
+public class Pen {
+    public static void main(String[] args) {
+        PenSetting myPen1 = new PenSetting("Red", 1);
+        PenSetting myPen2 = new PenSetting("Black", 2);
+
+        System.out.println("연필1의 색상은 " + myPen1.getColor() + "입니다.");
+        System.out.println("연필1은 " + myPen1.getNumber() + "개 입니다.");
+        System.out.println("#############################################");
+        System.out.println("연필2의 색상은 " + myPen2.getColor() + "입니다.");
+        System.out.println("연필2는 " + myPen2.getNumber()  + "개 입니다.");
+    }
+}
